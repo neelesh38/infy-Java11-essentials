22e2255d9f0982eb758997dbd4b87e09c02188a8
Neelesh Bansal <neeleshbbansal38@gmail.com>
2022-06-23 09:44:47 +0530

    Pacakge Access

---
+++ PackageandAccessModifiersDemo/src/com/infy/package1/DemoOne.java
@@ -0,0 +1,5 @@
+package com.infy.package1;
+
+public class DemoOne {
+
+}
---
+++ PackageandAccessModifiersDemo/src/com/infy/package2/DemoThree.java
@@ -0,0 +1,13 @@
+package com.infy.package2;
+
+import com.infy.package1.*;
+
+public class DemoThree {
+	public void checkAccessibility() {
+		DemoOne demoOne= new DemoOne();
+		demoOne.variableOne = 5;
+		
+		
+	}
+
+}
